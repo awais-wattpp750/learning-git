d5899f8c37f3482b30f46dc16b0644bab06288aa
awaiswattoo750 <muhammadawaiswatto750@gmail.com>
2026-07-04 11:56:52 +0500

    i add consol.log('git pull')

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ let number2=90;
 console.log(number1+number1);
 console.log(number1-number1+5);
 console.log("i am learnign the js and change in script.js from inviter");
+console.log("i am learning the git pull ");
