37f93233be44543367972d8ef8261cd9f5675205
Awais <awaiswattoo.awasi@gmail.com>
2026-07-05 09:20:56 +0500

    save my work in js file

--- script.js
+++ script.js
@@ -3,4 +3,3 @@ let number2=90;
 console.log(number1+number1);
 console.log(number1-number1+5);
 console.log("i am learnign the js and change in script.js from inviter");
-console.log("i am learning the git pull ");
